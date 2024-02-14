bfda466ab59de4a409279d3923f255be6c6db198
Vladislav Kornyakov <vladislav.kornyakov.blr@gmail.com>
2024-02-14 17:23:41 +0300

    Lesson 103

---
+++ 103/src/js/script.js
@@ -0,0 +1,11 @@
+import {one, two} from './main.js';
+import sayHello from './main.js';
+console.log(`There is ${one} and ${two}`);
+sayHello();
+
+// import {one as first} from './main';
+// console.log(first);
+
+// import * as data from './main';
+// console.log(`There is ${data.one} and ${data.two}`);
+// data.sayHello();
