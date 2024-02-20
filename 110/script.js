6468340a82b741ab30d0fdfa09d4793c669808f1
Vladislav Kornyakov <time2begin666@gmail.com>
2024-02-20 16:46:51 +0300

    Added comment

--- 110/script.js
+++ 110/script.js
@@ -8,11 +8,12 @@ $(function() {
     $('.list-item:eq(2)').on('click', function() {
         $('.image:even').fadeToggle('slow');
     });
-    
+
     $('.list-item:eq(4)').on('click', function() {
         $('.image:odd').animate({
             opacity: 'toggle',
             height: 'toggle'
         }, 2000);
     });
+    //Hey now I changed email
 });
