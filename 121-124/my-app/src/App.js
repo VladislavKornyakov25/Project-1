f5d3003e2ed84af99d08de1621e2468e6f55ce2d
Vladislav Kornyakov <time2begin666@gmail.com>
2024-03-01 19:13:53 +0300

    Lesson 130

--- 121-124/my-app/src/App.js
+++ 121-124/my-app/src/App.js
@@ -1,14 +1,43 @@
 import {Component, StrictMode}  from 'react';
 import './App.css';
 
-function WhoAmI({name, surname, link}) { // function WhoAmI(props) - old version
-	return (
-		<div>
-			<h1>My name is {name}, surname - {surname}</h1>
-			<a href={link}>My Profile</a>
-		</div>
-	);
+class WhoAmI extends Component { 
+	constructor (props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++'
+		}
+	}
+
+	nextYear = () => {
+		this.setState(state => ({
+			years: state.years + 1
+		}));
+		
+	}
+	render () {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>My Profile</a>
+			</div>
+		);
+	}
+	
 }
+
+
+// function WhoAmI({name, surname, link}) { // function WhoAmI(props) - old version
+// 	return (
+// 		<div>
+// 			<h1>My name is {name}, surname - {surname}</h1>
+// 			<a href={link}>My Profile</a>
+// 		</div>
+// 	);
+// }
 
 function App() {
   return (
