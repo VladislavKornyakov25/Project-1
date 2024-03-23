ca92907c8eb3a9cafc8948f266a7589b199a47f4
Vladislav Kornyakov <time2begin666@gmail.com>
2024-03-23 11:47:55 +0300

    Lesson 159

--- 121-124/my-app/src/App.js
+++ 121-124/my-app/src/App.js
@@ -1,137 +1,58 @@
-import React, {Component}  from 'react';
-import styled from 'styled-components';
-import BootstrapTest from './BootstrapTest';
-
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
-const EmpItem = styled.div`
-	padding: 20px;
-	margin-bottom: 15px;
-	border-radius: 5px;
-	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+class Form extends Component {
+    // constructor(props) {
+    //     super(props);
+	// 	this.myRef = React.createRef();
+    // }
 
-`;
+	// componentDidMount() {
+	// 	this.myRef.current.focus();
+	// }
 
-const Header = styled.h2`
-	font-size: 22px;
-`;
-
-const Button = styled.button`
-	display: block;
-	padding: 5px 15px;
-	background-color: gold;
-	border: 1px solid rgba(0,0,0, .2);
-	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
-`;
-
-class WhoAmI extends Component { 
-	constructor (props) {
-		super(props);
-		this.state = {
-			years: 27,
-			text: '+++',
-			position: ''
+	setInputRef = elem => {
+		this.myRef = elem;
+	}
+	focusFirstTI = () => {
+		if (this.myRef) {
+			this.myRef.focus();
 		}
 	}
 
-	nextYear = () => {
-		this.setState(state => ({
-			years: state.years + 1
-		}));
-		
-	}
-
-	commitInputChanges = (event, color) => {
-		
-		this.setState({
-			position: event.target.value
-		});
-	}
-
-	render () {
-		const {name, surname, link} = this.props;
-		const {years, position, text} = this.state;
-		return (
-			<EmpItem>			
-				<Button onClick={this.nextYear}>{text}</Button>
-				<Header>My name is {name}, surname - {surname}, age - {years}, position - {position}</Header>
-				<a href={link}>My Profile</a>
-				<form>
-					<span>
-						Введите должность
-					</span>
-					<input type="text" onChange={(event) => this.commitInputChanges(event, 'some color')}/>
-				</form>
-			</EmpItem>
-		);
-	}
-	
-}
-
-
-// function WhoAmI({name, surname, link}) { // function WhoAmI(props) - old version
-// 	return (
-// 		<div>
-// 			<h1>My name is {name}, surname - {surname}</h1>
-// 			<a href={link}>My Profile</a>
-// 		</div>
-// 	);
-// }
-const Wrapper = styled.div`
-	width: 600px;
-	margin: 80px auto 0px auto;	
-`;
-
-const DynamicGreating = (props) => {
-	return (
-		<div className={'mb-3 p-3 border border-' + props.color}>
-			{
-				React.Children.map(props.children, child => {
-					return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'});
-				})
-			}
-		</div>
-	)
-}
-
-const HelloGreatings = () => {
-	return (
-		<div style={{'width': '600px', 'margin': '0 auto'}}>
-			<DynamicGreating color={'primary'}>
-				<h2>Hello World</h2>				
-			</DynamicGreating> 
-		</div>
-	)
+    render() {
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input 
+							ref={this.setInputRef} 
+							type="email" 
+							className="form-control" 
+							id="exampleFormControlInput1" 
+							placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea 
+							onClick={this.focusFirstTI} 
+							className="form-control" 
+							id="exampleFormControlTextarea1" 
+							rows="3">								
+						</textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }
 }
 
 function App() {
-  return (
-    <Wrapper>   
-		<HelloGreatings/>
-
-		<BootstrapTest
-			left = {
-				<DynamicGreating color={'primary'}>
-					<h2>This weel was hard</h2>
-					<h2>Hello</h2>
-				</DynamicGreating> 
-			}
-			right = {
-				<DynamicGreating color={'primary'}>
-					<h2>Right</h2>
-					
-				</DynamicGreating> 
-			}
-		/> 
-	  	<WhoAmI 
-			name="John" 
-			surname="Smith"
-			link="facebook.com"/>	
-		<WhoAmI 
-			name="Alex" 
-			surname="Ivanovich"
-			link="facebook.com"/>
-    </Wrapper>
-  );
+    return (
+        <Form/>
+    );
 }
+
 export default App;
