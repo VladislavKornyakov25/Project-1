f6551914810f1fb03be98e22ead71f9c3310235e
Vladislav Kornyakov <time2begin666@gmail.com>
2024-03-25 12:48:59 +0300

    Created new learning folder

--- 115/script.js
+++ 115/script.js
@@ -6,3 +6,10 @@ Promise.resolve()
     .then(() => console.log('promise'));
 
 console.log('run code');
+
+
+var countBits = function(n) {
+    return console.log('--' + n.toString(2));// Program Me
+};
+
+console.log(countBits(2));
